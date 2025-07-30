251d6b90beb56b88111cf808b75a5524084d1ada
Lucas Stackhouse <stacked.house@MacBook-Air-114.local>
2025-07-29 23:17:44 -0600

    added db_create route to create bb table

--- app.py
+++ app.py
@@ -12,3 +12,21 @@ def db_test():
     conn = psycopg.connect("postgresql://lab10_db_73dc_user:k8B4vhJZNyKhqGS0mwQZa7hHECiYNCkO@dpg-d24qcrfgi27c73baq5f0-a/lab10_db_73dc")
     conn.close()
     return "Database connection successful!"
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg.connect("postgresql://lab10_db_73dc_user:k8B4vhJZNyKhqGS0mwQZa7hHECiYNCkO@dpg-d24qcrfgi27c73baq5f0-a/lab10_db_73dc")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+        );
+    ''')
+    conn.commit()
+    cur.close()
+    conn.close()
+    return "Basketball Table Created"
